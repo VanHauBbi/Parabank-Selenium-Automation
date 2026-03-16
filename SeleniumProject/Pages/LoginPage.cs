c495c008842f0062c5137e9999c317323588150e
sv <sv@HAPM03-PC15>
2026-03-16 11:51:30 +0700

    Add project files.

---
+++ SeleniumProject/Pages/LoginPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace SeleniumProject.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private By UsernameInput = By.Name("username");
+        private By PasswordInput = By.Name("password");
+        private By LoginButton = By.XPath("//input[@value='Log In']");
+
+        public void LoginToParabank(string username, string password)
+        {
+            _driver.FindElement(UsernameInput).SendKeys(username);
+            _driver.FindElement(PasswordInput).SendKeys(password);
+            _driver.FindElement(LoginButton).Click();
+        }
+    }
+}
---
+++ SeleniumProject/Utilities/ExcelHelper.cs
@@ -0,0 +1,52 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+
+namespace SeleniumProject.Utilities
+{
+    public class ExcelHelper
+    {
+        public static string GetTestDataPath()
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(baseDirectory, "TestData", "TestData.xlsx");
+        }
+
+        public static string ReadData(int row, int col)
+        {
+            Environment.SetEnvironmentVariable("EPPlusLicenseContext", "NonCommercial");
+            FileInfo fileInfo = new FileInfo(GetTestDataPath());
+
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                return worksheet.Cells[row, col].Value?.ToString() ?? "";
+            }
+        }
+
+        public static void WriteResult(int row, int statusCol, string status, int actualResultCol, string actualResult)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            FileInfo fileInfo = new FileInfo(GetTestDataPath());
+
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+                worksheet.Cells[row, statusCol].Value = status;
+                worksheet.Cells[row, actualResultCol].Value = actualResult;
+
+                if (status.ToUpper() == "PASS")
+                {
+                    worksheet.Cells[row, statusCol].Style.Font.Color.SetColor(System.Drawing.Color.Green);
+                }
+                else if (status.ToUpper() == "FAIL")
+                {
+                    worksheet.Cells[row, statusCol].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                }
+
+                package.Save();
+            }
+        }
+    }
+}
