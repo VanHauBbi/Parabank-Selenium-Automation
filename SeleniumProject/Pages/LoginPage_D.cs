f91e18ced557fc9eabf2c178e81e70a8a112c9d8
KienDiep2207 <jackbetfcavn@gmail.com>
2026-03-22 22:55:38 +0700

    Thêm bộ test case ParaBank
    Danh sách các Test Case:
    - TC01 & TC02: Mở tài khoản mới (Loại Checking và Savings).
    
    - TC03, TC10, TC11, TC13: Kiểm tra hiển thị tổng quan tài khoản (Số dư, Số dư khả dụng, Tổng kết).
    
    - TC04, TC16: Kiểm tra truy cập và hiển thị trang Quản trị (Admin Page).
    
    - TC05: Kiểm tra hiển thị loại tài khoản Savings trong danh sách.
    
    - TC19: Kiểm tra tính năng khởi tạo cơ sở dữ liệu (Initialize Database).

---
+++ SeleniumProject/Pages/LoginPage_D.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Pages
+{
+    public class LoginPage_D
+    {
+        IWebDriver driver;
+
+        public LoginPage_D(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement txtUsername => driver.FindElement(By.Name("username"));
+        IWebElement txtPassword => driver.FindElement(By.Name("password"));
+        IWebElement btnLogin => driver.FindElement(By.XPath("//input[@value='Log In']"));
+
+        public void Login(string username, string password)
+        {
+            txtUsername.SendKeys(username);
+            txtPassword.SendKeys(password);
+            btnLogin.Click();
+        }
+    }
+}
---
+++ SeleniumProject/Utilities/DriverFactory_D.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Utilities
+{
+    public class DriverFactory_D
+    {
+        public static IWebDriver InitDriver()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl(
+                "https://parabank.parasoft.com/parabank/index.htm"
+            );
+
+            return driver;
+        }
+    }
+}
