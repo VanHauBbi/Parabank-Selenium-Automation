22d77d459f814ae945873385597a0a36c81eacd5
VY MINH TOÀN <22dh114772@st.huflit.edu.vn>
2026-03-23 10:21:52 +0700

    loan

---
+++ SeleniumProject/Pages/AboutPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using SeleniumProject.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Pages
+{
+    public class AboutPage
+    {
+        private readonly IWebDriver _driver;
+
+        public AboutPage()
+        {
+            _driver = DriverFactory_Toan.GetDriver();
+        }
+
+        private IWebElement AboutUsLink => _driver.FindElement(By.LinkText("About Us"));
+        private IWebElement AboutUsTitle => _driver.FindElement(By.ClassName("title"));
+        private IWebElement SolutionLink => _driver.FindElement(By.LinkText("www.parasoft.com"));
+
+        public void NavigateTo()
+        {
+            AboutUsLink.Click();
+        }
+
+        public string GetPageTitle()
+        {
+            return AboutUsTitle.Text;
+        }
+
+        public string GetSolutionLinkUrl()
+        {
+            return SolutionLink.GetAttribute("href");
+        }
+    }
+
+}
---
+++ SeleniumProject/Pages/LoginPage_Toan.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using SeleniumProject.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Pages
+{
+    public class LoginPage_Toan
+    {
+        private readonly IWebDriver _driver;
+
+        public LoginPage_Toan()
+        {
+            _driver = DriverFactory_Toan.GetDriver();
+        }
+
+        // Định nghĩa các Elements (Selectors)
+        private IWebElement UsernameInput => _driver.FindElement(By.Name("username"));
+        private IWebElement PasswordInput => _driver.FindElement(By.Name("password"));
+        private IWebElement LoginButton => _driver.FindElement(By.XPath("//input[@value='Log In']"));
+        private IWebElement ErrorMessage => _driver.FindElement(By.ClassName("error"));
+
+
+        public void NavigateTo()
+        {
+            _driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/index.htm");
+        }
+
+        public void Login(string username, string password)
+        {
+            UsernameInput.Clear();
+            UsernameInput.SendKeys(username);
+            PasswordInput.Clear();
+            PasswordInput.SendKeys(password);
+            LoginButton.Click();
+        }
+
+        public string GetErrorMessage()
+        {
+            return ErrorMessage.Text;
+        }
+    }
+}
